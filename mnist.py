e98a0f3630363f62c71836d2d7a1255736859dcf
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-11 01:00:48 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -14,8 +14,9 @@ st.write('# ↓で書いてください')
 CANVAS_SIZE = 192
 
 col1, col2 = st.columns(2)
-mode = st.checkbox("消しますか", True)
+mode = st.checkbox("消しますか", False)
 mode = not bool(mode)  
+st.write('↑チェック入れると消しゴムモードになる')
 if mode == True:
    with col1:
     canvas = st_canvas(
